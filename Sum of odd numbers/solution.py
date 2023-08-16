b3b4a956f31221a33d74037eb02601d3ef65daf2
jonas <jonascastro.dev@gmail.com>
2023-08-15 21:16:31 -0300

    deletado erros

--- Sum of odd numbers/solution.py
+++
@@ -1,9 +0,0 @@
-def row_sum_odd_numbers(n):
-    soma = 0
-    for i in range(n):
-        if i % 2 != 0:
-            soma += i
-            print (soma)
-    return soma
-
-row_sum_odd_numbers(13)
